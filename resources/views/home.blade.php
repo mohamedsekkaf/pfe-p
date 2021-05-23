313db9797f8c8f7aaacf4e0a8087fe301be8ff17
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2021-05-23 01:50:59 +0100

    cec

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,46 @@
 @extends('header')
 @section('content')
 <div class="">
+    <div>
+        <div class="dropdown show">
+            <a class="btn fas fa-bars " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Menu
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="btn3" href="{{url('/home')}}" style="text-decoration:none; color:black;">Accueil</a><br><br>
+                <a class="btn3" href="{{url('/details/Département économique')}}" style="text-decoration:none; color:black;">
+                    Département économique</a><br><br>
+                <a class="btn3" href="{{url('/details/Département de l environnement')}}"
+                    style="text-decoration:none; color:black;">Département de l'environnement</a><br><br>
+                <a class="btn3" href="{{url('/details/département de santé')}}"
+                    style="text-decoration:none; color:black;">département de santé </a><br><br>
+                <a class="btn3" href="{{url('/details/Département des sports')}}"
+                    style="text-decoration:none; color:black;">Département des sports</a><br><br>
+                <a class="btn3" href="{{url('details/Département de la police administrative')}}"
+                    style="text-decoration:none; color:black;">Département de la police administrative</a><br><br>
+            </div>
+        </div>
+    </div>
     <div class="left">
         <br>
         <button class="btn1"><a href="{{url('/home')}}" style="text-decoration:none; color:black;">Accueil</a> </button>
         <br><br>
-        <button class="btn1"><a href="{{url('/details/Département économique')}}" style="text-decoration:none; color:black;"> Département économique</a></button>
+        <button class="btn1"><a href="{{url('/details/Département économique')}}"
+                style="text-decoration:none; color:black;"> Département économique</a></button>
         <br><br>
-        <button class="btn1"> <a href="{{url('/details/Département de l environnement')}}" style="text-decoration:none; color:black;">Département de l'environnement</a></button>
+        <button class="btn1"> <a href="{{url('/details/Département de l environnement')}}"
+                style="text-decoration:none; color:black;">Département de l'environnement</a></button>
         <br><br>
-        <button class="btn1"> <a href="{{url('/details/département de santé')}}" style="text-decoration:none; color:black;">département de santé</a></button>
+        <button class="btn1"> <a href="{{url('/details/département de santé')}}"
+                style="text-decoration:none; color:black;">département de santé</a></button>
         <br><br>
-        <button class="btn1"> <a href="{{url('/details/Département des sports')}}" style="text-decoration:none; color:black;">Département des sports</a></button>
+        <button class="btn1"> <a href="{{url('/details/Département des sports')}}"
+                style="text-decoration:none; color:black;">Département des sports</a></button>
         <br><br>
-        <button class="btn1"> <a href="{{url('details/Département de la police administrative')}}" style="text-decoration:none; color:black;">Département de la police administrative</a></button>
+        <button class="btn1"> <a href="{{url('details/Département de la police administrative')}}"
+                style="text-decoration:none; color:black;">Département de la police administrative</a></button>
     </div>
     <div class="main">
         <h1 class="title">Les Réclamation</h1>
@@ -21,14 +48,14 @@
         <br>
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title"><i style="color:red">Sujet</i>  : {{$ch->reclamation}}</h4>
+                <h4 class="card-title"><i style="color:red">Sujet</i> : {{$ch->reclamation}}</h4>
                 <table>
                     <tr>
                         <td> <img class="chikaya1" src="{{asset('image/chikaya.png')}}" alt="chikaya"></td>
                         <td>
                             <table>
                                 <tr>
-                                    <td > <i style="color:green;">Nom et Prénom</i>  : {{$ch->nom}} {{$ch->prenom}}</td>
+                                    <td> <i style="color:green;">Nom et Prénom</i> : {{$ch->nom}} {{$ch->prenom}}</td>
                                     <td><i style="color:green;">CIN</i> : {{$ch->cin}}</td>
                                 </tr>
                                 <tr>
